6e0bcea3285abf589cc831acfe97b1b753663b8e
GU Junhua <jhgu@nao.cas.cn>
2022-10-20 12:12:46 +0800

    new file:   src/bin/ampl_resp_2stages2.rs 	new file:   src/ospfb2.rs

---
+++ src/bin/ampl_resp_2stages2.rs
@@ -0,0 +1,162 @@
+extern crate rsdsp;
+
+use std::fs::File;
+
+use rsdsp::{
+    ampl_resp::ampl_resp_2stages_1freq2,
+    cfg::{PfbCfg, TwoStageCfg},
+    windowed_fir::pfb_coeff,
+};
+
+use num::traits::FloatConst;
+
+use serde_yaml::from_reader;
+
+use clap::{Arg, Command};
+
+use ndarray::{parallel::prelude::*, Array1, Array2, ArrayView1, Axis};
+
+use itertools_num::linspace;
+
+use ndarray_npy::NpzWriter;
+
+type FloatType = f64;
+
+pub fn main() {
+    let matches = Command::new("ampl_resp_2stages")
+        .arg(
+            Arg::new("chcfg")
+                .short('c')
+                .long("cfg")
+                .takes_value(true)
+                .value_name("config file")
+                .required(true),
+        )
+        .arg(
+            Arg::new("fmin")
+                .short('f')
+                .long("fmin")
+                .allow_hyphen_values(true)
+                .takes_value(true)
+                .value_name("freq")
+                .default_value("-1")
+                .required(false),
+        )
+        .arg(
+            Arg::new("fmax")
+                .short('F')
+                .long("fmax")
+                .allow_hyphen_values(true)
+                .takes_value(true)
+                .value_name("freq")
+                .default_value("1")
+                .required(false),
+        )
+        .arg(
+            Arg::new("nfreq")
+                .short('n')
+                .long("nfreq")
+                .takes_value(true)
+                .value_name("nfreq")
+                .default_value("1024")
+                .required(false),
+        )
+        .arg(
+            Arg::new("niter")
+                .short('t')
+                .long("niter")
+                .takes_value(true)
+                .value_name("niter")
+                .default_value("2")
+                .required(false),
+        )
+        .arg(
+            Arg::new("outfile")
+                .short('o')
+                .long("out")
+                .takes_value(true)
+                .value_name("output name")
+                .required(true),
+        )
+        .get_matches();
+
+    let mut cfg_file = File::open(matches.value_of("chcfg").unwrap()).unwrap();
+    let TwoStageCfg {
+        coarse_cfg:
+            PfbCfg {
+                nch: nch_coarse,
+                k: k_coarse,
+                tap_per_ch: tap_coarse,
+            },
+        fine_cfg:
+            PfbCfg {
+                nch: nch_fine,
+                k: k_fine,
+                tap_per_ch: tap_fine,
+            },
+        selected_coarse_ch,
+    } = from_reader(&mut cfg_file).unwrap();
+
+    let fmin = matches
+        .value_of("fmin")
+        .unwrap()
+        .parse::<FloatType>()
+        .unwrap();
+    let fmax = matches
+        .value_of("fmax")
+        .unwrap()
+        .parse::<FloatType>()
+        .unwrap();
+    let nfreq = matches.value_of("nfreq").unwrap().parse::<usize>().unwrap();
+    let niter = matches.value_of("niter").unwrap().parse::<usize>().unwrap();
+
+    let coeff_coarse =
+        pfb_coeff::<FloatType>(nch_coarse, tap_coarse, 1.5*k_coarse as FloatType).into_raw_vec();
+    let coeff_fine =
+        pfb_coeff::<FloatType>(nch_fine * 2, tap_fine, k_fine as FloatType).into_raw_vec();
+
+    let signal_len = coeff_coarse.len() + coeff_fine.len() * nch_coarse / 2;
+    println!("signal length={}", signal_len);
+    let bandwidth = (fmax - fmin) * FloatType::PI();
+    let df = bandwidth / (nfreq + 1) as FloatType;
+    let freqs = Array1::from(
+        linspace(FloatType::PI() * fmin, FloatType::PI() * fmax - df, nfreq).collect::<Vec<_>>(),
+    );
+    let mut coarse_spec = Array2::<FloatType>::zeros((nfreq, selected_coarse_ch.len()));
+    let mut fine_spec = Array2::<FloatType>::zeros((nfreq, selected_coarse_ch.len() * nch_fine));
+    println!("{:?}", freqs);
+
+    fine_spec
+        .axis_iter_mut(Axis(0))
+        .into_par_iter()
+        .zip_eq(coarse_spec.axis_iter_mut(Axis(0)).into_par_iter())
+        .zip_eq(freqs.axis_iter(Axis(0)).into_par_iter())
+        .for_each(|((mut fine_resp, mut coarse_resp), freq)| {
+            let freq = freq[()];
+
+            let (coarse_resp1, fine_resp1) = ampl_resp_2stages_1freq2(
+                nch_coarse,
+                nch_fine,
+                &coeff_coarse,
+                &coeff_fine,
+                &selected_coarse_ch,
+                freq,
+                signal_len,
+                niter,
+            );
+
+            coarse_resp.assign(&ArrayView1::from(&coarse_resp1));
+            fine_resp.assign(&ArrayView1::from(&fine_resp1));
+        });
+
+    let outfile = std::fs::File::create(matches.value_of("outfile").unwrap()).unwrap();
+    let mut npz = NpzWriter::new(outfile);
+    npz.add_array("freq", &freqs).unwrap();
+    npz.add_array("coarse", &coarse_spec).unwrap();
+    npz.add_array("fine", &fine_spec).unwrap();
+    npz.add_array(
+        "coarse_ch",
+        &ArrayView1::from(&selected_coarse_ch).map(|&x| x as i32),
+    )
+    .unwrap();
+}
---
+++ src/ospfb2.rs
@@ -0,0 +1,126 @@
+//! oversampling poly phase filter bank
+
+#![allow(clippy::uninit_vec)]
+use crate::cspfb;
+use ndarray::{s, Array2, ArrayView1, Axis, ScalarOperand};
+use num::{
+    complex::Complex,
+    traits::{Float, FloatConst, NumAssign},
+};
+
+use serde::{Deserialize, Serialize};
+
+use rustfft::{FftNum};
+use std::{iter::Sum, ops::Mul};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Analyzer<R, T>
+where
+    R: std::fmt::Debug,
+    T: std::fmt::Debug + Float,
+{
+    /// filters for even channels
+    pfb_even: cspfb::Analyzer<R, T>,
+
+    /// filters for odd channels
+    pfb_odd: cspfb::Analyzer<R, T>,
+}
+
+impl<R, T> Analyzer<R, T>
+where
+    T: Copy
+        + Float
+        + FloatConst
+        + ScalarOperand
+        + NumAssign
+        + std::fmt::Debug
+        + Sync
+        + Send
+        + FftNum,
+    R: Copy
+        + Mul<T, Output = R>
+        + Default
+        + ScalarOperand
+        + NumAssign
+        + std::fmt::Debug
+        + Sum
+        + Sync
+        + Send,
+    Complex<T>: Copy + std::convert::From<R> + Sum + Default + ScalarOperand,
+{
+    /// constructor
+    /// * `nch_total` - total number of channels, including even and odd, pos and neg channels
+    /// * `coeff` - property low pass filter coefficients, the length of which should be equal to `nch_total`/2*`tap_per_ch`
+    /// ```
+    /// extern crate rsdsp;
+    /// use num::complex::Complex;
+    /// use rsdsp::{
+    ///     windowed_fir
+    ///     , ospfb::Analyzer
+    /// };
+    ///
+    /// let nch=32;
+    /// let tap_per_ch=16;
+    /// let k=1.1;
+    /// let coeff=windowed_fir::pfb_coeff::<f64>(nch/2, tap_per_ch, k);
+    /// let mut pfb=Analyzer::<Complex<f64>, f64>::new(nch, coeff.view());
+    /// ```
+    pub fn new(nch: usize, coeff: ArrayView1<T>) -> Self {
+        let tap = coeff.len() / nch;
+        assert!(nch * tap == coeff.len());
+
+        let mut pfb_even = cspfb::Analyzer::new(nch, coeff);
+        let pfb_odd = cspfb::Analyzer::new(nch, coeff);
+
+        let x0 = vec![R::default(); nch / 2];
+        pfb_even.feed(&x0);
+
+        Self { pfb_even, pfb_odd }
+    }
+
+    pub fn nch_total(&self) -> usize {
+        self.pfb_even.nch() * 2
+    }
+
+    pub fn analyze(&mut self, input_signal: &[R]) -> Array2<Complex<T>> {
+        let y_even = self.pfb_even.analyze_raw(input_signal);
+        let mut y_odd = self.pfb_odd.analyze_raw(input_signal);
+
+        y_odd
+            .axis_iter_mut(Axis(1))
+            .skip(1)
+            .step_by(2)
+            .for_each(|mut y| y.map_inplace(|x| *x *= -T::one()));
+
+        //println!("{:?}", y_even.slice(s![..,1]).map(|x|x.re));
+        //println!("{:?}", y_odd.slice(s![..,1]).map(|x|x.re));
+        let batch = y_even.shape()[0] * 2;
+        let nch = y_even.shape()[1];
+        assert_eq!(y_even.shape(), y_odd.shape());
+        let mut result = unsafe { Array2::<Complex<T>>::uninit((batch, nch)).assume_init() };
+        result.slice_mut(s![0_usize..;2,..]).assign(&y_even);
+        result.slice_mut(s![1_usize..;2,..]).assign(&y_odd);
+        result.t().as_standard_layout().to_owned()
+    }
+
+    pub fn analyze_par(&mut self, input_signal: &[R]) -> Array2<Complex<T>> {
+        let y_even = self.pfb_even.analyze_raw_par(input_signal);
+        let mut y_odd = self.pfb_odd.analyze_raw_par(input_signal);
+
+        y_odd
+            .axis_iter_mut(Axis(1))
+            .skip(1)
+            .step_by(2)
+            .for_each(|mut y| y.map_inplace(|x| *x *= -T::one()));
+
+        //println!("{:?}", y_even.slice(s![..,1]).map(|x|x.re));
+        //println!("{:?}", y_odd.slice(s![..,1]).map(|x|x.re));
+        let batch = y_even.shape()[0] * 2;
+        let nch = y_even.shape()[1];
+        assert_eq!(y_even.shape(), y_odd.shape());
+        let mut result = unsafe { Array2::<Complex<T>>::uninit((batch, nch)).assume_init() };
+        result.slice_mut(s![0_usize..;2,..]).assign(&y_even);
+        result.slice_mut(s![1_usize..;2,..]).assign(&y_odd);
+        result.t().as_standard_layout().to_owned()
+    }
+}
